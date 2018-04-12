f823ed4364e82ef9d5b688e435bb11c52948d96c
Priyank77 <pkshadow77@gmail.com>
2018-04-12 19:49:01 -0400

    Add files via upload
    Change Password for the App

---
+++ ChangePasswordActivity.java
@@ -0,0 +1,93 @@
+package com.example.pocketdate;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.password_dialog_layout);
+
+        final EditText password1 = (EditText) findViewById(R.id.EditText_Pwd1);
+        final EditText password2 = (EditText) findViewById(R.id.EditText_Pwd2);
+
+        Button button = (Button) findViewById(R.id.go_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(password1.getText().toString().equals(password2.getText().toString()))
+                {
+                    Intent myIntent = new Intent(ChangePasswordActivity.this, ProfileActivity.class);
+                    startActivity(myIntent);
+                }
+                else
+                {
+                    password2.setBackgroundColor(0x26FF0000);
+                    Toast.makeText(ChangePasswordActivity.this, "Passwords do not match. Please re-enter", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        password1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    if(password1.getText().toString().equals(password2.getText().toString()))
+                    {
+                        password2.setBackgroundColor(0x2600FF00);
+                    }
+                    else
+                    {
+                        password2.setBackgroundColor(0x26FF0000);
+                    }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                    if(password1.getText().toString().equals(password2.getText().toString()))
+                    {
+                        password2.setBackgroundColor(0x2600FF00);
+                    }
+            }
+        });
+
+        password2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(password2.getText().toString().equals(password1.getText().toString()))
+                {
+                    password2.setBackgroundColor(0x2600FF00);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                if(!(password2.getText().toString().equals(password1.getText().toString())))
+                {
+                    password2.setBackgroundColor(0x26FF0000);
+                }
+            }
+        });
+    }
+}
--- ProfileActivity.java
+++ ProfileActivity.java
@@ -1,27 +1,30 @@
 package com.example.pocketdate;
 
-import android.annotation.TargetApi;
-import android.content.Context;
+
+import android.content.CursorLoader;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.example.stupidcupid.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -34,88 +37,183 @@ import java.util.List;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class ProfileActivity extends AppCompatPreferenceActivity {
+public class ProfileActivity extends AppCompatPreferenceActivity  {
     private static final String TAG = ProfileActivity.class.getSimpleName();
+
+    int userID;
+    String userEmail;
+    String profileLocation;
+    String firstName;
+    String lastName;
+    boolean inChat;
+    private boolean changeHappened;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // creates a back button on the actionbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent thisActivity = getIntent();
 
+        // change happened represents whether or not the user changed any personal information while on this page
+        // if they did, then we need to refresh the mainactivity page when we return. otherwise, onBackPressed() will suffice
+        this.changeHappened = false;
+        // this is a bad way to do this (use bundles and checks instead of directly accessing data from intent)
+        this.userID = thisActivity.getIntExtra("userID", -1);
+        this.userEmail = thisActivity.getStringExtra("inputEmail");
+        this.profileLocation = thisActivity.getStringExtra("profileLocation");
+        this.firstName = thisActivity.getStringExtra("firstName");
+        this.lastName = thisActivity.getStringExtra("lastName");
+        this.inChat = thisActivity.getBooleanExtra("inChat", true);
         //load profile fragment
         getFragmentManager().beginTransaction().replace(android.R.id.content, new ProfilePreferenceFragment()).commit();
+
     }
 
+    // this is called after the file manager activity ends
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        // must call the super method here because we stated the activity from the preference fragment
+        // and we want the data to be returned to the activity class
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // if the result is okay, then go ahead and process the data
+        if (resultCode != RESULT_CANCELED){
+            if(data != null)
+            {
+                // grabs the uri from the file selection activity
+                Uri targetUri = data.getData();
+                String uriPath = getRealPathFromURI(targetUri);
+                // create the stream variable that we will read the image in from
+                final InputStream imageStream;
+
+                // tries to get a base64 representation of the image
+                try {
+                    imageStream = getContentResolver().openInputStream(targetUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    String encodedImage = encodeImage(selectedImage);
+                    // establishes a connectino to our remote server where our php code will process the uploading of the image
+                    ServerConnection myServer = new ServerConnection("http://cop4331groupeight.com/chatapi.php");
+                    String resultString = myServer.updateProfilePic(encodedImage, this.userID);
+                    Toast.makeText(getApplicationContext(), "Successfully updated profile picture", Toast.LENGTH_SHORT).show();
+                    // logs that a change was made -- this is for the back button
+                    this.changeHappened = true;
+                    JSONArray resultJSON = new JSONArray(resultString);
+                    JSONObject jsonObj = resultJSON.getJSONObject(0);
+                    this.profileLocation = jsonObj.getString("profileLocation");
+                // handles the case of the file uri being invalid or an invalid response from the php code
+                } catch (FileNotFoundException | JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error loading image. Please try again.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            // in case we can't load the image for some reason
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Error loading image. Please try again.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // method that will take the bitmap of an image and convert it into a base64 string
+    private String encodeImage(Bitmap bm)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG,100,baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+
+        return encImage;
+    }
+
+    // method that will take a uri and convert it into a valid filepath string
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    // preference fragment
     public static class ProfilePreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
+
+            // grabs the preference item that will activate the method for changing a user's profile image
+            Preference myPref = (Preference) findPreference("change_prof_pic");
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    // in onCreate or any event where your want the user to  select a file
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    // call main activity's startActivityForResult function (allows us to return data to main activity)
+                    // the 2 is an arbitrary request code. value just needs to be something greater than 0
+                    getActivity().startActivityForResult(intent, 2);
+                    return true;
+                }
+            });
+
+            final EditTextPreference editPref = (EditTextPreference) findPreference("about_text");
+            editPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                   editPref.setSummary(editPref.getText());
+                   return true;
+                }
+            });
+
+            final ListPreference genderPref = (ListPreference) findPreference("gender_pref");
+            genderPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    genderPref.setSummary(genderPref.getEntry());
+                    return true;
+                }
+            });
+
+            Preference changePref =  (Preference) findPreference("change_password");
+            changePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent myIntent = new Intent(getActivity(), ChangePasswordActivity.class);
+                    startActivity(myIntent);
+                    return true;
+                }
+            });
         }
     }
 
+    // if the user selects the back button, process the request depending on whether or not any data was altered (profile pic change, etc.)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
+
+            // if there was a change to the profile information, we need to refresh the main activity
+            if(changeHappened)
+            {
+                Intent returnToMain = new Intent(this, MatchActivity.class);
+                returnToMain.putExtra("userID", userID);
+                returnToMain.putExtra("inputEmail", userEmail);
+                returnToMain.putExtra("profileLocation", profileLocation);
+                returnToMain.putExtra("firstName", firstName);
+                returnToMain.putExtra("lastName", lastName);
+                returnToMain.putExtra("inChat", inChat);
+                // restarts the main activity
+                startActivity(returnToMain);
+            }
+            // otherwise, just pop this activity off of the stack and go back to the previous activity, unchanged
+            else
+            {
+                onBackPressed();
+            }
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-            } else if (preference instanceof RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent);
-
-                } else {
-                    Ringtone ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue));
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(null);
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        String name = ringtone.getTitle(preference.getContext());
-                        preference.setSummary(name);
-                    }
-                }
-
-            } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-    };
 }
-
-    /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
